4deede965add9d3c554e2c473becbc217ac4ddb0
nohjuhyeon <njh2720@gmail.com>
2023-12-29 12:34:18 +0900

    readme

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 
 # 역할 분담
-## function 1 : 클릭해서 상세 정보 들어가기, function 2: 나가기  
-## function 1 : 스크롤 하기 or 페이지 넘기기, function 2 : 리뷰 가져오기 및 DB에 올리기
+## function 1 : 클릭해서 상세 정보 들어가기, function 2: 나가기 function 3 : 페이지 넘기기
+## function 1 : 페이지 넘기기, function 2 : 리뷰 가져오기 및 DB에 올리기
 
 # Mongo DB
 ## client : mongodb://192.168.10.240:27017/
 ## Database : toy_seleniums
-## collection : 
+## collection : kurly_review 
---
+++ docs/kurly_main.py
@@ -0,0 +1,22 @@
+import kurly_get_review_function as review
+import kurly_select_item_function as select
+def main(): # self 키워드 필요 (class 소속 확인용)
+    try: 
+        pass        # 업무 코드
+    except:
+        pass        # 업무 코드 문제 발생 시 대처 코드
+    finally:
+        pass       # try나 except이 끝난 후 무조건 실행 코드
+    pass    # 내용 넣기
+    return 0
+
+if __name__ == "__main__":
+    try: 
+        main()        # 업무 코드
+    except:
+        pass        # 업무 코드 문제 발생 시 대처 코드
+    finally:
+        pass        # try나 except이 끝난 후 무조건 실행 코드
+
+    pass
+
